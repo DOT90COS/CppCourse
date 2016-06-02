8774c4cdbc5d69d36663bcdeea297b1f4a0824bc
Jeff <Jeff@Scaparra.com>
2016-06-01 23:21:40 -0500

    initial add of containers lab.

---
+++ Assignments/Containers/Containers/SubCipherEncrypt/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+//Add container headers that you want to use here
+
+////////////////////////////////////////////////
+
+/**************************************************
+Write an application that will:
+Encode a subsitution cipher encoded message
+
+* You program should take 2 parameters
+* 1st - encrypted message filename
+* 2nd - the filename containing the key
+
+It should decode the message and output to a file name
+the encrypted files name appended with .clear.txt
+***************************************************/
+int main(int argc, char* argv[])
+{
+
+
+}
