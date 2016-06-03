3fa6b1464d3cbc42ddc3e2786d1529510137b0da
Jeff <Jeff@Scaparra.com>
2016-06-03 12:57:55 -0500

    Updated assignment.

--- Assignments/04-functional-more-containers/lambdas/Source.cpp
+++ Assignments/04-functional-more-containers/lambdas/Source.cpp
@@ -5,12 +5,12 @@
 
 /*
 All comparitors comp(a,b):
-
-* For all a, comp(a,a) == false
-* If comp(a,b) == true, then comp(b, a) == false
-* If comp(a,b) == true and comp(b,c) == true, then comp(a,c) == true
-
-*/
+ 
+  * For all a, comp(a,a) == false
+  * If comp(a,b) == true, then comp(b, a) == false
+  * If comp(a,b) == true and comp(b,c) == true, then comp(a,c) == true
+  
+  */
 
 //Less typing! :)
 using uint = unsigned int; // == typedef unsigned int uint;
@@ -31,29 +31,37 @@ int main()
 {
     const std::vector<uint> dataset = { 10,2,2503,3,5,64,4095,9086,12402,0xDEADBEEF,0765432,9 };
 
-    //Pass a lambda into sort that will put these in increasing order
     std::cout << "Increasing order: \n";
     auto increasingOrder(dataset); //a copy
-    std::sort(increasingOrder.begin(), increasingOrder.end(), /*LAMBDA GOES HERE*/);
+    std::sort(increasingOrder.begin(), increasingOrder.end(), /*LAMBDA HERE*/);
     printVec(increasingOrder);
 
-    //Pass a lambda into sort that will put these in decreasing order
     std::cout << "Decreasing order: \n";
     auto decreasingOrder(dataset);
-    std:First.begin(), evenFirst.end(), evenOverOdd);
-    printVec(evenFirst);
+    std::sort(decreasingOrder.begin(), decreasingOrder.end(), /*LAMBDA HERE*/);
+    printVec(decreasingOrder);
 
-    //all odd numbers infront of even ones and inside of both of those sets the numbers are sorted.
+    std::cout << "Odd First: \n";
+    auto oddFirst(dataset);
+    auto oddOverEven = /*LAMBDA HERE*/;
+    std::sort(oddFirst.begin(), oddFirst.end(), oddOverEven);
+    printVec(oddFirst);
+    
+    std::cout << "Even First: \n";
+    auto evenFirst(dataset);
+    auto evenOverOdd =  /*LAMBDA HERE*/;
+    std::sort(evenFirst.begin(), evenFirst.end(), evenOverOdd);
+    printVec(evenFirst);
+    
     std::cout << "Increasing Odd First: \n";
     auto increasingOddFirst(dataset);
-    auto oddOverEvenIncrease =  /*You may reuse your lambdas from above inside this one. Just think about how you access them*/;
+    auto oddOverEvenIncrease = /*LAMBDA HERE*/;
     std::sort(increasingOddFirst.begin(), increasingOddFirst.end(), oddOverEvenIncrease);
     printVec(increasingOddFirst);
-
-    //All even numbers infront of odd ones and inside of both of those sets the numbers are sorted.
+   
     std::cout << "Increasing Even First: \n";
     auto increasingEvenFirst(dataset);
-    auto evenOverOddIncrease =  /*The inverse of above*/;
+    auto evenOverOddIncrease = /*LAMBDA HERE*/
     std::sort(increasingEvenFirst.begin(), increasingEvenFirst.end(), evenOverOddIncrease);
     printVec(increasingEvenFirst);
 
